a7af7f27fc79a10bf2ba8a9de35848a041fea08a
Victor Henrique <vhsdm7@gmail.com>
2022-07-03 22:02:17 -0300

    adicionado a entrada do usuario para criar o tabuleiro

--- src/br/com/campoMinado/app/App.java
+++ src/br/com/campoMinado/app/App.java
@@ -1,13 +1,29 @@
 package br.com.campoMinado.app;
 
+import java.util.Scanner;
+
 import br.com.campoMinado.model.Tabuleiro;
 import br.com.campoMinado.model.TabuleiroConsole;
 
 public class App {
 	public static void main(String[] args) {
-
-		Tabuleiro tabuleiro = new Tabuleiro(6, 6, 6);
-		new TabuleiroConsole(tabuleiro);
+		Scanner entrada = new Scanner(System.in);
 		
+		int linha = 0;
+		int coluna = 0;
+		int mina = 0;
+		
+		System.out.print("Informe a quantidade de Linhas: ");
+		linha = entrada.nextInt();
+		System.out.print("\nInforme a quantidade de Colunas: ");
+		coluna = entrada.nextInt();
+		System.out.print("\nInforme a quantidade de Minas: ");
+		mina = entrada.nextInt();
+		
+		Tabuleiro tabuleiro = new Tabuleiro(linha, coluna, mina);
+		new TabuleiroConsole(tabuleiro);
+	
+		entrada.close();
+
 	}
 }
